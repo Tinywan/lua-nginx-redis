4de0393ae0e5abafe2cf55b406bf518f15e70b02
tinywan <756684177@qq.com>
2017-04-25 23:21:31 +0800

    php Socket

---
+++ PHP/Socket/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tinywan
+ * Date: 2017/4/25
+ * Time: 21:22
+ */
+
+
+/**
+ * 创建并返回一个套接字，也称作一个通讯节点
+ * resource socket_create ( int $domain , int $type , int $protocol )
+ */
+socket_create($domain,$typ,$protocol);
